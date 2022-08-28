792923485d765ee791d72c75519b2c61b2640519
abubakr <mkrishmishra78@gmail.com>
2022-08-29 01:38:19 +0530

    CRUD operation for notes model

--- routes/notes.js
+++ routes/notes.js
@@ -26,5 +26,34 @@ router.post('/addnote', fetchUser, [
 
 })
 
+router.put('/updatenote/:id', fetchUser, async(req, res) => {
+    const {title, description, tag} = req.body
+    const nodeData = {}
+    if(title) nodeData.title = title
+    if(description) nodeData.description = description
+    if(tag) nodeData.tag = tag
+
+    let note = await Note.findById(req.params.id);
+    if(!note) return res.status(400).json({error: 'not found'})
+    if(note.user.toString() !== req.user.id)
+    {
+        return res.status(400).json({error: 'not found'})
+    }
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: nodeData}, {new:true})
+    res.json(note)
+})
+
+
+router.delete('/deletenote/:id', fetchUser, async(req, res) => {
+    
+    let note = await Note.findById(req.params.id);
+    if(!note) return res.status(400).json({error: 'not found'})
+    if(note.user.toString() !== req.user.id)
+    {
+        return res.status(401).json({error: 'Not allowed'})
+    }
+    note = await Note.findByIdAndDelete(req.params.id)
+    res.json(note)
+})
 
 module.exports = router;
